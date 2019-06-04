5dba3d5a51e72664e7f7d53ec1b7fa8648fe2c80
qiupeng <bmcciscoding@gmail.com>
2019-06-04 23:40:50 +0800

    pod

--- Example/SwiftExtensions/ViewController.swift
+++ Example/SwiftExtensions/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import SwiftExtensions
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIApplication.shared.qp.cacheURL
         
     }
 
