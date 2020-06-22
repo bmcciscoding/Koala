eaedc39e58f9067e8ca0d896468c68cfbecc20c7
qiupeng <bmcciscoding@gmail.com>
2020-06-22 16:27:31 +0800

    init project && reddit oauth code

--- Koala.podspec
+++ Koala.podspec
@@ -28,8 +28,8 @@ Pod::Spec.new do |s|
   s.source           = { :git => 'https://github.com/bmcciscoding/Koala.git', :tag => s.version.to_s }
   # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.ios.deployment_target = '9.0'
-  s.platform = :ios, '9.0'
+  s.ios.deployment_target = '10.0'
+  s.platform = :ios, '10.0'
   s.swift_version = '4.0'
   
   s.source_files = 'Koala/Classes/**/*'
---
+++ Koala/Classes/UIKit/UIWindow+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  UIWindow.swift
+//  Koala
+//
+//  Created by qiupeng on 2020/6/22.
+//
+
+import Foundation
+
+public extension UIWindow {
+    class var keyWindow: UIWindow {
+        return UIApplication.shared.keyWindow!
+    }
+}
+
+public extension UIViewController {
+    
+    func present(view: UIView) {
+        let vc = UIViewController()
+        vc.view.addSubview(view)
+        self.present(vc, animated: true, completion: nil)
+    }
+    
+}
