0c4465ba77e2f37d92133a15872d22a7b80bad4f
qiupeng <bmcciscoding@gmail.com>
2020-06-22 14:57:52 +0800

    add uiwindow

--- Koala/Classes/SwiftType/BaseType.swift
+++ Koala/Classes/SwiftType/BaseType.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public extension Optional {
+    var isNil: Bool {
+        switch self {
+            case .none: return true
+            case .some(_): return false
+        }
+    }
+}
+
 // MARK: - Int
 extension Int: KoalaNamespaceable {}
 
---
+++ Koala/Classes/UIKit/UIWindow.swift
@@ -0,0 +1,14 @@
+//
+//  UIWindow.swift
+//  Koala
+//
+//  Created by qiupeng on 2020/6/22.
+//
+
+import Foundation
+
+public extension UIWindow {
+    class var keyWindow: UIWindow {
+        return UIApplication.shared.keyWindow!
+    }
+}
