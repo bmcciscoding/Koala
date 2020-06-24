26866645ab64660383d46fb765cbf75cfdf2abf0
qiupeng <bmcciscoding@gmail.com>
2020-06-24 11:46:12 +0800

    init photo list

---
+++ KoalaConstant.swift
@@ -0,0 +1,13 @@
+//
+//  KoalaConstant.swift
+//  Koala
+//
+//  Created by qiupeng on 2020/6/24.
+//
+
+import Foundation
+
+
+// MARK: UI
+let SCREEN_WIDTH = UIScreen.main.bounds.width
+let SCREEN_HEIGHT = UIScreen.main.bounds.height
