985f9fbeeb1aeaa761db121591a674b43e003b3c
qiupeng <bmcciscoding@gmail.com>
2020-06-29 16:59:50 +0800

    update keywindow logic

--- Koala/Classes/UIKit/UIWindow+Ext.swift
+++ Koala/Classes/UIKit/UIWindow+Ext.swift
@@ -9,7 +9,13 @@ import Foundation
 
 public extension KoalaNamespace where T == UIWindow {
     static var keyWindow: UIWindow {
-        return UIApplication.shared.keyWindow!
+        if let win = UIApplication.shared.keyWindow {
+            return win
+        }
+        if let win = UIApplication.shared.delegate?.window, let w = win {
+            return w
+        }
+        return UIApplication.shared.windows.first ?? UIWindow.init(frame: UIScreen.main.bounds)
     }
 }
 
